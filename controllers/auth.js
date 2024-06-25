5a17068fd88323cd4f4b28913bcb6bc4df06dfcc
donmutalib <backendkolawole@gmail.com>
2024-06-25 17:06:22 +0100

    modify register and login controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,7 @@ const register = async (req, res)=> {
     }
     const user = await User.create(req.body)
     const token = await user.createJWT()
-    res.status(StatusCodes.CREATED).json({user: {name: user.name}, token})
+    return res.status(StatusCodes.CREATED).json({user: {name: user.name, email: user.email, lastName: user.lastName, location: user.location, token}})
 }
 
 const login = async (req, res)=> {
@@ -29,7 +29,8 @@ const login = async (req, res)=> {
     }
     const token = await user.createJWT()
 
-    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+    return res.status(StatusCodes.OK).json({user: {name: user.name, email: user.email, lastName: user.lastName, location: user.location, token}})
+
 }
 
 
