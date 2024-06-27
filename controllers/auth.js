dc7aa9d92c159841503a6ada999ffe10e0e1f302
donmutalib <backendkolawole@gmail.com>
2024-06-27 04:28:56 +0100

    update user controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,6 +33,27 @@ const login = async (req, res)=> {
 
 }
 
+const updateUser = async (req, res) => {
+    const {email, name, lastName, location} = req.body
+
+    if (!email || !name || !lastName || !location) {
+        return res.status(400).json({msg: `Please provide all values`})
+    }
+
+    const user = await User.findOne({_id: req.user.userId})
+
+    user.email = email
+    user.name = name
+    user.lastName = lastName
+    user.location = location
+
+    await user.save()
+    const token = user.createJWT()
+    
+    return res.status(StatusCodes.OK).json({user: {name: user.name, email: user.email, lastName: user.lastName, location: user.location, token}})
+
+}
 
 
-module.exports = {login, register}
+
+module.exports = {login, register, updateUser}
